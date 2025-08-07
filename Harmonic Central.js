7a0d4388e55dcdd50c5f574aa39c95f86f4a00ed
Nuriel Altshtein <nurihnuri@gmail.com>
2025-08-07 06:15:17 +0300

    pdp part 1

--- Harmonic Central.js
+++ Harmonic Central.js
@@ -60,20 +60,41 @@ function changeSideCatalogFunction(event) {
 
 // הכנסת מוצרים מג'ייסון לקטלוג 
 fetch('../data/products.json')
-
-.then(response => response.json())
-.then(products => {
-    products.forEach(product => {
-        creator(product);
-    });
-})
-.catch(error => console.error('error in json:', error));
+    .then(response => response.json())
+    .then(products => {
+            // בדיקת כתובת אתר וקריאה לפונקצית בניית דף מוצר
+            if(window.location.href.includes('Product%20Page.html')){
+                const urlId = window.location.href.split('=')[2].split('&')[0];
+                const product = products.find(productId => productId.id === urlId);
+                if(product){
+                    pdpCreator(product);
+                }
+                else{
+                    console.warn('id is not found', urlId);
+                }
+            }
+            // במידה וזה לא דף מוצר - יצירת קטלוג
+            else{
+                products.forEach(product => {
+                creator(product);
+            });
+        }
+    })
+    .catch(error => console.error('error in json:', error));
 
 // פונקציה ראשית המפעילה את כל הפונקציות לאחר קבלת נתונים מהשרת
 function creator(product){
+    const currentPath = window.location.pathname;
 
-    // קריאה לפונקציה של בניית כרטיס מוצר
-    productCreator(product);
+    // דף הבית
+    if(currentPath.includes('index.html')){
+        productCreator(product);
+    }
+    
+    // דף קטלוג
+    else if(currentPath.includes('catalog')){
+        catalogProductCreator(product);
+    }
 
     //  השמה של אירוע על האייקונים שבתוך כרטיסיית המוצר על מנת להוסיף למועדפים ולעגלה
     const allProducts = document.querySelectorAll('.catalog_product_box').forEach(product => {
@@ -93,6 +114,129 @@ function creator(product){
 
 // בניית כרטיסיית מוצר 
 function productCreator(product){
+
+    // יצירת קופסא ראשית למוצר
+    const productBox = document.createElement('div');
+    productBox.classList.add('product_box');
+    productBox.id = product.id;
+    productBox.setAttribute("data-stock", product.stock || 1);
+
+    // יצירת תיבת התמונה
+    const productImgBox = document.createElement('div');
+    productImgBox.classList.add('product_img_box');
+
+    const productImg = document.createElement('img');
+    productImg.classList.add('product_img');
+    productImg.src = product.img;
+
+    productImgBox.appendChild(productImg);
+
+    // יצירת הפס העליון (לוגו + אייקונים)
+    const productTopBar = document.createElement('div');
+    productTopBar.classList.add('product_top_bar');
+
+    const productLogoImg = document.createElement('img');
+    productLogoImg.classList.add('product_logo_img');
+    productLogoImg.src = product.logo;
+
+    const productAction = document.createElement('div');
+    productAction.classList.add('product_action');
+
+    const heartIcon = document.createElement('i');
+    heartIcon.classList.add('fa-solid', 'fa-heart', 'heart_icon')
+
+    const cartIcon = document.createElement('i');
+    cartIcon.classList.add('fa-solid', 'fa-cart-shopping', 'cart_icon');
+
+    productAction.appendChild(heartIcon);
+    productAction.appendChild(cartIcon);
+
+    productTopBar.appendChild(productLogoImg);
+    productTopBar.appendChild(productAction);
+
+    // יצירת תיבת מידע 
+    const info = document.createElement('div');
+    info.classList.add('info');
+
+    // כותרת מוצר
+    const h3Box = document.createElement('div');
+    h3Box.classList.add('h3_box');
+
+    const productName = document.createElement('h3');
+    productName.classList.add('product_name')
+    productName.textContent = product.name;
+    h3Box.appendChild(productName);
+
+    // תיאור ממוצר
+    const descriptionBox = document.createElement('div');
+    descriptionBox.classList.add('description_box');
+
+    const description = document.createElement('p');
+    description.classList.add('description');
+    description.textContent = product.description;
+
+    descriptionBox.appendChild(description);
+
+    // יצירת הפס התחתון (מחיר + כפתור)
+    const productBottomBar = document.createElement('div');
+    productBottomBar.classList.add('product_bottom_bar');
+
+    // תיבת מחיר 
+    const priceBox = document.createElement('div');
+    priceBox.classList.add('price_box');
+
+    const originalPriceBox = document.createElement('div');
+    originalPriceBox.classList.add('original_price_box');
+
+    const originalPrice = document.createElement('h4');
+    originalPrice.classList.add('original_price');
+    originalPrice.textContent = product.originalPrice;
+
+    const discount = document.createElement('p');
+    discount.classList.add('discount');
+    discount.textContent = product.discount;
+
+    originalPriceBox.appendChild(originalPrice);
+    originalPriceBox.appendChild(discount);
+
+    const finalPrice = document.createElement('h2');
+    finalPrice.classList.add('cost');
+    finalPrice.textContent = product.finalPrice;
+
+    priceBox.appendChild(originalPriceBox);
+    priceBox.appendChild(finalPrice);
+
+    // כפתור קניה
+    const buyButton = document.createElement('button');
+    buyButton.classList.add('buy');
+
+    const buyLink = document.createElement('a');
+    buyLink.href = `../product page/Product Page.html?category=${encodeURIComponent(product.category)}&id=${product.id}&name=${encodeURIComponent(product.name)}`;
+    buyLink.textContent = 'Buy Now';
+
+    buyButton.appendChild(buyLink);
+
+    // חיבור החלקים 
+    productBottomBar.appendChild(priceBox);
+    productBottomBar.appendChild(buyButton);
+
+    info.appendChild(h3Box);
+    info.appendChild(descriptionBox);
+    info.appendChild(productBottomBar);
+
+    productBox.appendChild(productImgBox);
+    productBox.appendChild(productTopBar);
+    productBox.appendChild(info);
+
+    // הכנסת כרטיס מוצר לדום
+    const container = document.querySelector('#sale_products');
+    if (container){
+        container.appendChild(productBox);
+    }
+}
+
+// בניית כרטיסיית מוצר לקטלוג
+function catalogProductCreator(product){
 
     // יצירת קופסא ראשית למוצר
     const catalogProductBox = document.createElement('div');
@@ -190,7 +334,7 @@ function productCreator(product){
     buyButton.classList.add('buy');
 
     const buyLink = document.createElement('a');
-    buyLink.href = product.link;
+    buyLink.href = `../product page/Product Page.html?category=${encodeURIComponent(product.category)}&id=${product.id}&name=${encodeURIComponent(product.name)}`;
     buyLink.textContent = 'Buy Now';
 
     buyButton.appendChild(buyLink);
@@ -221,13 +365,18 @@ function productCreator(product){
             container.appendChild(catalogProductBox);
         }    
     }
-
 }
 
-// רענון המוצרים בווישליסט בעת רענון או טעינת הדף 
+// השמה של ארוע שמרענן את הדפים
 document.addEventListener('DOMContentLoaded', () => {
     getItemsFromWishlist();
     getItemsFromCart();
+
+    // בדיקת כתובת אתר וקריאה לפונקצית בניית דף מוצר
+    if(window.location.href.includes('Product%20Page.html')){
+        const urlId = window.location.href.split('=')[2].split('&')[0];
+        pdpCreator(product);
+    }
 });
 
 // שמירת הנתונים של המוצר בלוקאלסטורג' למועדפים + צביעה של אייקון לב באדום
@@ -328,6 +477,10 @@ function getItemsFromWishlist() {
     if(qtyInWishlist){
         qtyInWishlist.textContent = 'Total: ' + getWishlist.length + ' items in your Wishlist';
     }
+
+
+    // זימון פונקציה של מחיר כולל של המוצרים 
+    updateSubtotal('wishlist', '.wishlist_subtotal');
 }
 
 // בניית כרטיס מועדפים 
@@ -455,6 +608,8 @@ function removeFromWishlist(event){
     if(qtyInWishlist){
         qtyInWishlist.textContent = 'Total: ' + wishlist.length + ' items in your Wishlist';
     }
+
+    updateSubtotal('wishlist', '.wishlist_subtotal');
 }
 
 // שמירת הנתונים של המוצר בלוקאלסטורג' לעגלה + צביעה של אייקון עגלה באדום
@@ -481,6 +636,7 @@ function saveToCart(event){
     const finalPrice = productBox.querySelector('.cost')?.textContent.trim();
     const stock = parseInt(productBox.dataset.stock) || 1;
     const buyLink = productBox.querySelector('.buy')?.href;
+    const qty = 1;
 
     // מבנה נתונים לשמירה
     const cartProductData = {
@@ -493,7 +649,8 @@ function saveToCart(event){
         discount: discount,
         finalPrice: finalPrice,
         stock: stock,
-        buyLink: buyLink
+        buyLink: buyLink,
+        qty: qty
     };
 
 
@@ -556,6 +713,10 @@ function getItemsFromCart() {
     if(qtyInCart){
         qtyInCart.textContent = 'Total: ' + getCart.length + ' items in your cart';
     }
+
+    eventOnQty(); 
+    updateSubtotal();
+    updateSubtotal('cart', '.payment_subtotal');
 }
 
 // בניית כרטיס עגלה 
@@ -590,18 +751,22 @@ function cartCreator(cartProductData){
     originalPriceBox.classList.add('cart_original_price_box');
 
     const originalPrice = document.createElement('h4');
-    originalPrice.textContent = cartProductData.originalPrice;
+    originalPrice.textContent = formatPrice(parsePrice(cartProductData.originalPrice));
 
     const discount = document.createElement('p');
-    discount.textContent = cartProductData.discount;
+    discount.textContent = cartProductData.discount || '';
 
     originalPriceBox.appendChild(originalPrice);
     originalPriceBox.appendChild(discount);
 
     const finalPrice = document.createElement('h2');
     finalPrice.classList.add('cart_cost');
-    finalPrice.textContent = cartProductData.finalPrice;
 
+    const unitPrice = parsePrice(cartProductData.finalPrice);
+    const qty = Number(cartProductData.qty) || 1;
+    const totalPrice = unitPrice * qty;
+
+    finalPrice.textContent = formatPrice(totalPrice); 
     priceBox.appendChild(originalPriceBox);
     priceBox.appendChild(finalPrice);
 
@@ -626,8 +791,9 @@ function cartCreator(cartProductData){
     qtyLabel.textContent = 'Qty';
 
     const qtyInput = document.createElement('input');
+    qtyInput.classList.add('qty_input');
     qtyInput.type = 'number';
-    qtyInput.value = cartProductData.quantity || 1;
+    qtyInput.value = cartProductData.qty || 1;
     qtyInput.min = 1;
 
     qtyBox.appendChild(qtyLabel);
@@ -689,4 +855,141 @@ function removeFromCart(event){
     if(qtyInCart){
         qtyInCart.textContent = 'Total: ' + cart.length + ' items in your cart';
     }
+
+    updateSubtotal();
+    updateSubtotal('cart', '.payment_subtotal');
 }
+
+// הוספת אירוע על כמות מוצר לרכישה
+function eventOnQty() {
+    const qtyInputs = document.querySelectorAll('.qty_input');
+    qtyInputs.forEach(input => {
+        input.addEventListener('input', cartQty);
+    });
+}
+
+// פונקציה לשינוי המחיר לפי כמות בעגלה
+function cartQty(event) {
+    const qtyInput =  event.currentTarget;
+    const qtyValue = Number(qtyInput.value);
+    const cartProductBox = qtyInput.closest('.cart_product_box');
+
+    if(!cartProductBox){
+        return;
+    }
+
+    // עדכון זיכרון לפי תעודת זהות
+    const productId = cartProductBox.closest('li')?.getAttribute('data-id');
+    if(!productId){
+        return;
+    }
+    
+    // עדכון כמות בזיכרון 
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    const index = cart.findIndex(item => item.id === productId);
+    if (index !== -1) {
+        cart[index].qty = qtyValue;
+        localStorage.setItem('cart', JSON.stringify(cart));
+    }
+
+    //  עדכון המחיר בכרטיס מוצר לפי כמות
+    const finalPriceEl = cartProductBox.querySelector('.cart_cost');
+    if (finalPriceEl) {
+        const pricePerItem = parseFloat(cart[index].finalPrice.replace('$', '').trim()) || 0;
+        const totalPrice = pricePerItem * qtyValue;
+        finalPriceEl.textContent = totalPrice.toFixed(2) + ' $';
+    }
+
+    // עדכון מחיר סופי
+    updateSubtotal();
+    updateSubtotal('cart', '.payment_subtotal');
+}
+
+function updateSubtotal(storageKey = 'cart', selector = '.cart_subtotal') {
+    const data = JSON.parse(localStorage.getItem(storageKey)) || [];
+
+    let total = 0;
+    data.forEach(item => {
+        const price = parsePrice(item.finalPrice);
+        const qty = Number(item.qty) || 1;
+        total += price * qty;
+    });
+
+    const priceDisplay = document.querySelector(selector); 
+    if (priceDisplay) {
+        priceDisplay.textContent = formatPrice(total);
+    }
+}
+
+// המרה של מחרוזת עם פסיקים למספר תקני
+function parsePrice(str) {
+    return parseFloat(str.replace(/,/g, '').trim()) || 0;
+}
+
+// המרה של מספר למחרוזת עם פסיקים ו-2 ספרות אחרי נקודה
+function formatPrice(num) {
+    return num.toLocaleString('en-US', {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    }) + ' $';
+}
+
+// יצירת דף מוצר
+function pdpCreator(product){
+    // תמונה ראשית לדף מוצר 
+    const mainImage = document.createElement('img');
+    mainImage.classList.add('product_preview_img');
+    mainImage.src = product.img;
+    document.querySelector('#product_preview').appendChild(mainImage);
+
+    // תמונה ראשונה לדף מוצר 
+    const firstImage = document.createElement('img');
+    firstImage.classList.add('first_image');
+    firstImage.src = product.images [0];
+    document.querySelector('#first_gallery_item').appendChild(firstImage);
+
+    // תמונה שניה לדף מוצר 
+    const secondImage = document.createElement('img');
+    secondImage.classList.add('second_image');
+    secondImage.src = product.images[1];
+    document.querySelector('#second_gallery_item').appendChild(secondImage);
+
+    // תמונה שלישית לדף מוצר 
+    const thirdImage = document.createElement('img');
+    thirdImage.classList.add('third_image');
+    thirdImage.src = product.images[2];
+    document.querySelector('#third_gallery_item').appendChild(thirdImage);
+
+    // תמונה רביעית לדף מוצר 
+    const fourthImage = document.createElement('img');
+    fourthImage.classList.add('fourth_image');
+    fourthImage.src = product.images[3];
+    document.querySelector('#fouth_gallery_item').appendChild(fourthImage);
+
+    // תמונה חמישית לדף מוצר 
+    const fifthImage = document.createElement('img');
+    fifthImage.classList.add('fifth_image');
+    fifthImage.src = product.images[4];
+    document.querySelector('#fifth_gallery_item').appendChild(fifthImage);
+
+    // שם מוצר
+    const pdpName = document.querySelector('#pdp_name')
+    pdpName.textContent = product.name;
+
+    // מחיר מקורי
+    const originalPrice = document.querySelector('#pdp_original_price');
+    originalPrice.textContent = product.originalPrice;
+
+    // הנחה
+    const discount = document.querySelector('#pdp_discount');
+    discount.textContent = product.discount;
+
+    // מחיר סופי
+    const finalPrice = document.querySelector('#final_price');
+    finalPrice.textContent = product.finalPrice;
+
+    // תיאור מוצר
+    const description = document.querySelector('#pdp_description');
+    description.textContent = product.description;
+}
+
